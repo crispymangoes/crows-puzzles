5a59a9d5f35b82ef31c7526cd849263f48d7afc5
crispymangoes <crispymangoes@gmail.com>
2022-09-09 14:56:51 -0700

    Change guess to string, and use enumerable sets to store all active prizes

--- src/PuzzleMaster.sol
+++ src/PuzzleMaster.sol
@@ -5,13 +5,16 @@ import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerklePr
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract PuzzleMaster is Ownable{
     using SafeERC20 for ERC20;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
 
     bytes32 public root;
     ERC20 immutable public prizeToken;
-
+    
+    EnumerableSet.Bytes32Set private activePrizes;
     mapping(bytes32 => uint256) public prizes;
 
     constructor(bytes32 _root, ERC20 _prizeToken) {
@@ -25,7 +28,7 @@ contract PuzzleMaster is Ownable{
 
     error PuzzleMaster__PrizeAlreadySet();
 
-    function claimPrize(bytes32[] memory proof, bytes32 leaf, uint256 guess) external {
+    function claimPrize(bytes32[] memory proof, bytes32 leaf, string memory guess) external {
         // Skip merkle proof check if root is zero.
         if(root != bytes32(0) && !MerkleProof.verify(proof, root, leaf)) revert PuzzleMaster__InvalidProof();
 
@@ -34,6 +37,7 @@ contract PuzzleMaster is Ownable{
 
         if(prizeAmount > 0){
             delete prizes[prizeHash];
+            activePrizes.remove(prizeHash);
             prizeToken.transfer(msg.sender, prizeAmount);
         }
         else revert PuzzleMaster__NoPrize();
@@ -43,11 +47,13 @@ contract PuzzleMaster is Ownable{
         if(prizes[prizeHash] > 0) revert PuzzleMaster__PrizeAlreadySet();
         prizeToken.transferFrom(msg.sender, address(this), prizeAmount);
         prizes[prizeHash] = prizeAmount;
+        activePrizes.add(prizeHash);
     }
 
     function removePrize(bytes32 prizeHash) external onlyOwner{
         uint256 prizeAmount = prizes[prizeHash];
         delete prizes[prizeHash];
+        activePrizes.remove(prizeHash);
         prizeToken.transferFrom(address(this), msg.sender, prizeAmount);
     }
 
@@ -60,12 +66,23 @@ contract PuzzleMaster is Ownable{
         selfdestruct(payable(msg.sender));
     }
 
-    function getPrizeAmount(uint256 guess) external view returns(uint256 prizeAmount){
+    function getPrizeAmount(string memory guess) external view returns(uint256 prizeAmount){
         bytes32 prizeHash = keccak256(abi.encode(guess));
         prizeAmount = prizes[prizeHash];
     }
 
-    function getHash(uint256 guess) external pure returns(bytes32) {
+    function getHash(string memory guess) external pure returns(bytes32) {
         return keccak256(abi.encode(guess));
+    }
+
+    function getActivePrizes() external view returns(bytes32[] memory allPrizes, uint256[] memory amounts) {
+        uint256 len = activePrizes.length();
+        allPrizes = new bytes32[](len);
+        amounts = new uint256[](len);
+        for(uint256 i=0; i<len; i++) {
+            bytes32 prizeHash = activePrizes.at(i);
+            allPrizes[i] = prizeHash;
+            amounts[i] = prizes[prizeHash];
+        }
     }
 }
